2561bbf54654a563c87efcf3e246551aec13038a
escuderopc <escuderop.carina@gmail.com>
2022-07-06 20:48:34 -0500

    feat: ejemplo de ejecucion desde code runner

--- 1-node/1.2-crear-archivo/index.js
+++ 1-node/1.2-crear-archivo/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
-const fileName = 'my-file.txt';
+//code runner - se ejecuta en la raiz del proyecto
+const fileName = './js-avanzado-carina/1-node/1.2-crear-archivo/my-file.txt';
+// con la terminal normal 
+//const fileName = 'my-file.txt'
 const textContent = 'Este texto lo escribi desde node';
 fs.writeFile(fileName, textContent, (err) => {
     if (err){
