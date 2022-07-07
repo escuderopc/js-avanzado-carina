732de5aaf9b9d1ecbb48603552b4981469afb432
escuderopc <escuderop.carina@gmail.com>
2022-07-06 22:11:46 -0500

    feat: mi primer servidor en node

--- 1-node/1.3-primer-servidor/index.js
+++ 1-node/1.3-primer-servidor/index.js
@@ -4,10 +4,10 @@ const server = http.createServer((request, response) => {
     const status = 200;
     const mimeType = { 'Content-Type' : 'text/html' } ;
     response.writeHead(status, mimeType);
-    response.write('<html> <body> Adios </body> </html>');
+    response.write('<html> <body> Hola </body> </html>');
 });
 
-server.listen(8000);
+server.listen(8080);
 console.log('Inicie el servidor...')
 
 
