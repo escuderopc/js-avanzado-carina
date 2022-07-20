c12a4bbc2f4afd3fdb6da62118777d1bed92207e
escuderopc <escuderop.carina@gmail.com>
2022-07-20 17:49:15 -0500

    feat: proyecto npm para api express

---
+++ 3-express-js/3.1-shoes-api/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const colors = require('colors');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('Servidor express listening...'.rainbow);
+});
+
+let shoes = [
+    { id: 1, brand: 'noke', price: 200, size: 29 },
+    { id: 2, brand: 'edidas', price: 500, size: 22 },
+    { id: 3, brand: 'floxi', price: 900, size: 25 },
+];
+
+app.get('/', (req, res) => {
+    res.send('hola mundo!');
+});
+
+// http://localhost:3000/shoes/
+// Query Params: Filtrar información
+// http://localhost:3000/shoes/?page=1&pageSize=10&brand=%22noke%22
+// %20 => espacio en blanco
+// %22 => comillas dobles
+app.get('/shoes', (req, res) => {
+    const { page, pageSize, brand } = req.query;
+    if (page && pageSize && brand) {
+        res.json({ page, pageSize, brand });
+    } 
+    res.json(shoes);
+});
+
+// Request Param: Son utilizados para ejecutar operaciones sobre un elemento especifico
+// http://localhost:3000/shoes/999
+app.get('/shoes/:id', (req, res) => {
+    const { id } = req.params;
+    const shoe = { id: 1, brand: 'noke', price: 200, size: 29, searching: id };
+    res.json(shoe);
+});
+
+// http://localhost:3000/shoes/
+// { id: 4, brand: 'pima', price: 600, size: 26 }
+app.post('/shoes', (req, res) => {
+    const newShoe = req.body;
+    shoes.push(newShoe);
+    console.log(shoes);
+    const response = { message: 'shoe created!'};
+    res.status(201).json(response);
+});
+
+// PARTIAL EDITION: PATCH
+app.patch('/shoes/:id', (req, res) => {
+    const body = req.body;
+    const { id } = req.params;
+    const indexFounded = shoes.findIndex(shoe => shoe.id === id);
+    if (indexFounded !== -1) {
+        const shoeCopy = { ...shoes[indexFounded] };
+        shoes[indexFounded] = { ...shoeCopy, ...body };
+        res.json({ message: 'modified with success!', body });
+    } else {
+        res.send('ese id no existe');
+    }
+});
+
+// COMPLETE EDITION: PUT
+app.put('/shoes/:id', (req, res) => {
+    const body = req.body;
+    const { id } = req.params;
+    const indexFounded = shoes.findIndex(shoe => shoe.id === id);
+    if (indexFounded !== -1) {
+        const shoeCopy = { ...shoes[indexFounded] };
+        shoes[indexFounded] = { ...shoeCopy, ...body };
+        res.json({ message: 'modified with success!', body });
+    } else {
+        res.send('ese id no existe');
+    }
+});
